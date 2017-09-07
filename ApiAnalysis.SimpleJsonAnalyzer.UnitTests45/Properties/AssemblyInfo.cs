d88a772a1d0f5de0fb5b0e46200822f100afdaf1
Matt Lacey <matt@mrlacey.co.uk>
2017-09-07 21:08:31 +0100

    Added version of test project targetting .Net4.5

---
+++ ApiAnalysis.SimpleJsonAnalyzer.UnitTests45/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+// <copyright file="AssemblyInfo.cs" company="Matt Lacey">
+// Copyright (c) Matt Lacey. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the solution root for license information.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ApiAnalysis.SimpleJsonAnalyzer.UnitTests45")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ApiAnalysis.SimpleJsonAnalyzer.UnitTests45")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bcc08e7e-2b0d-4cbb-ab3a-66f7bf170867")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
